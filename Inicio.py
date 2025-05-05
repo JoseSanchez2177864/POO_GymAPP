1852b7803b0b9b51b90202b8dc4d3b316fecf9e1
José Pablo Sánchez Saucedo <pablo.sanchezs@uanl.edu.mx>
2025-05-04 22:19:19 -0600

    Se arregla los diseños de sesión y se agrega el inicio

--- Inicio.py
+++ Inicio.py
@@ -4,7 +4,7 @@ from kivy.uix.screenmanager import Screen
 from kivy.lang import Builder
 
 # Cargamos el KV
-Builder.load_file("InS.kv")
+Builder.load_file("Inicio.kv")
 
 # Definimos la primera pantalla
 class Iniciop(Screen):

c949620c1b74f2e5d4adb36ad370869acdf4ff6e
José Pablo Sánchez Saucedo <pablo.sanchezs@uanl.edu.mx>
2025-05-06 00:49:19 -0600

    Se añadió todo lo de Inicio de Sesión

--- InS.py
+++ InS.py
@@ -1,12 +1,31 @@
-# app.py
+import bcrypt
 from kivy.app import App
-from kivy.uix.screenmanager import Screen
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
+from kivy.uix.popup import Popup
+from kivy.uix.label import Label
+from ConBD import crear_conexion
 
-# Cargamos el KV
 Builder.load_file("InS.kv")
 
-# Definimos la primera pantalla
 class InSp(Screen):
-    pass
+    def verificar_credenciales(self):
+        nombre = self.ids.nombre_input.text
+        contrasena = self.ids.contrasena_input.text
 
+        conn = crear_conexion()
+        cur = conn.cursor()
+        cur.execute("SELECT Contrasena FROM usuarios WHERE NombreUsuario = ?", (nombre,))
+        row = cur.fetchone()
+        conn.close()
+        print(contrasena)
+        if row and contrasena == row[0]:
+            self.manager.current = "pantalla3"
+        else:
+            self.mostrar_error("Usuario o contraseña incorrecta")
+
+    def mostrar_error(self, mensaje):
+        popup = Popup(title='Error de inicio de sesión',
+                      content=Label(text=mensaje),
+                      size_hint=(None, None), size=(400, 200))
+        popup.open()
